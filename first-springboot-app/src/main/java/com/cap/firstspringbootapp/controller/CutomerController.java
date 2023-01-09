fc9f6870cb4e6a91da76150958536ab29a8a50c3
Nov-Java-Batch <mail2venky001@gmail.com>
2023-01-09 08:40:41 +0530

    added sub of three numbers

--- first-springboot-app/src/main/java/com/cap/firstspringbootapp/controller/CutomerController.java
+++ first-springboot-app/src/main/java/com/cap/firstspringbootapp/controller/CutomerController.java
@@ -4,31 +4,29 @@ package com.cap.firstspringbootapp.controller;
  * this is customer controller class
  */
 public class CutomerController {
-	
-	
+
 	int mul(int a, int b) {
-		
-		
+
 		return a * b;
 	}
-	
+
 	int div(int a, int b) {
-		
-		return a /b;
+
+		return a / b;
 	}
-	
-	
+
 	int add(int a, int b) {
-		
-		
-		
+
 		return a + b;
 	}
-	
-	
+
 	int sub(int a, int b) {
-		
-		
+
 		return a - b;
+	}
+
+	int sub(int a, int b, int c) {
+
+		return a - b - c;
 	}
 }
