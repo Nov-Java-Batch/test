9a280c4e5255f6f4f0ccc9145091f55174708abf
Nov-Java-Batch <mail2venky001@gmail.com>
2023-01-09 08:20:08 +0530

    added addition and substarction functionalities

--- first-springboot-app/src/main/java/com/cap/firstspringbootapp/controller/CutomerController.java
+++ first-springboot-app/src/main/java/com/cap/firstspringbootapp/controller/CutomerController.java
@@ -4,5 +4,19 @@ package com.cap.firstspringbootapp.controller;
  * this is customer controller class
  */
 public class CutomerController {
-
+	
+	
+	int add(int a, int b) {
+		
+		
+		
+		return a + b;
+	}
+	
+	
+	int sub(int a, int b) {
+		
+		
+		return a - b;
+	}
 }
