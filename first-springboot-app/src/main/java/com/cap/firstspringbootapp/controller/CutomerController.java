3ceb3f8e899370d9a35a17dee0e7b482250c2410
Nov-Java-Batch <mail2venky001@gmail.com>
2023-01-09 08:25:41 +0530

    some changes on same file

--- first-springboot-app/src/main/java/com/cap/firstspringbootapp/controller/CutomerController.java
+++ first-springboot-app/src/main/java/com/cap/firstspringbootapp/controller/CutomerController.java
@@ -4,5 +4,17 @@ package com.cap.firstspringbootapp.controller;
  * this is customer controller class
  */
 public class CutomerController {
+	
+	
+	int mul(int a, int b) {
+		
+		
+		return a * b;
+	}
+	
+	int div(int a, int b) {
+		
+		return a /b;
+	}
 
 }
